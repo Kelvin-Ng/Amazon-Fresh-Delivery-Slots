141dd1759340d0791514588f4cd30e23987c183b
Kelvin Ng <nkwkelvin@gmail.com>
2020-04-22 18:59:34 -0400

    Show the slots from all available days

--- amazon_fresh_delivery_slot_chrome.py
+++ amazon_fresh_delivery_slot_chrome.py
@@ -23,26 +23,37 @@ def getSlots(productUrl):
     while True:
         driver.refresh()
         time.sleep(sleep_for + random.randint(-sleep_for_rand_range, sleep_for_rand_range))
- 
-        slots = driver.find_elements_by_xpath('/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[1]/div[4]/div[2]/div/div[4]/div[2]/div/ul/li[1]/span/span/div')
-        if len(slots) == 0:
-            continue
 
         msg = ''
-        for slot in slots:
-            try:
-                button = slot.find_element_by_xpath('div[2]/span/span/button/div/span')
+
+        date_buttons = driver.find_elements_by_xpath('/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[1]/div[3]/div[2]/div/ul/li[1]/span/span/span/button/div')
+
+        for date_i, date_button in enumerate(date_buttons):
+            if date_button.find_element_by_xpath('div[3]').text == 'Not available':
+                continue
+
+            date_button.click()
+
+            slots = driver.find_elements_by_xpath('/html/body/div[5]/div[1]/div/div[2]/div/div/div/div/div[1]/div[4]/div[2]/div/div[4]/div[2]/div/ul/li[1]/span/span/div')
+            if len(slots) == 0:
+                continue
+
+            msg += '\n' + date_button.find_element_by_xpath('div[2]').text + ':'
+
+            for slot in slots:
                 try:
-                    time_str = slot.find_element_by_xpath('div[1]/div[1]/span').text
-                    msg += '\n' + time_str
+                    button = slot.find_element_by_xpath('div[2]/span/span/button/div/span')
+                    try:
+                        time_str = slot.find_element_by_xpath('div[1]/div[1]/span').text
+                        msg += '\n' + time_str
+                    except NoSuchElementException:
+                        pass
                 except NoSuchElementException:
                     pass
-            except NoSuchElementException:
-                pass
 
-        print('SLOTS OPEN!' + msg)
-
-        os.system('notify-send -a \'Amazon Fresh\' \'Amazon Fresh\' \'Delivery slots are available: {}\''.format(msg))
+        if msg != '':
+            print('Delivery slots available:' + msg)
+            os.system('notify-send -a \'Amazon Fresh\' \'Amazon Fresh\' \'Delivery slots available: {}\''.format(msg))
 
 getSlots('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
 
